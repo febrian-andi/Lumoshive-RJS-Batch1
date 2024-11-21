4147dce95353af9b928a42b223a69c522f1b2853
Febrian Andi Nugroho <febrianandinugroho@gmail.com>
2024-11-21 15:20:00 +0700

    task day 34

---
+++ Day 34 - Redux-toolkit (21 November 2024)/fe-react-redux-practice-todolist/src/App.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { changeLanguage } from "./redux/slices/languageSlice";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+
+const App = () => {
+  const { language } = useSelector((state) => state.language);
+  const dispatch = useDispatch();
+
+  const handleChangeLanguage = (language) => {
+    dispatch(changeLanguage(language));
+  };
+
+  return (
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card shadow">
+            <div className="card-body">
+              <h1 className="card-title text-center mb-4">
+                {language === "english" ? "To-Do List" : "Daftar Tugas"}
+              </h1>
+              <select
+                className="form-select mb-3"
+                value={language}
+                onChange={(e) => handleChangeLanguage(e.target.value)}
+              >
+                <option value="english">English</option>
+                <option value="indonesian">Indonesian</option>
+              </select>
+              <TodoInput />
+              <TodoList />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ Day 34 - Redux-toolkit (21 November 2024)/fe-react-redux-practice-todolist/src/components/TodoList.jsx
@@ -0,0 +1,86 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+// import { deleteTodo, toggleTodo, currentTodo } from "../redux/slices/todosSlice";
+import { fetchTodos, deleteTodo, getTodoById, toggleTodo } from "../redux/async/todosSlice";
+
+const TodoList = () => {
+  const dispatch = useDispatch();
+  const { todos, loading, error, isSuccess } = useSelector((state) => state.todos);
+  const { language } = useSelector((state) => state.language);
+
+  const handleDeleteTodo = (id, e) => {
+    dispatch(deleteTodo(id), e.stopPropagation());
+  };
+
+  const handleToggleTodo = (id) => {
+    dispatch(toggleTodo(id));
+  };
+
+  const handleCurrentTodo = (id, e) => {
+    // dispatch(currentTodo(todo), e.stopPropagation());
+    dispatch(getTodoById(id), e.stopPropagation());
+  };
+
+  useEffect(() => {
+    dispatch(fetchTodos());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isSuccess) {
+      dispatch(fetchTodos());
+    }
+  }, [isSuccess]);
+  
+  if (loading) {
+    return <div className="alert alert-secondary text-center">Loading...</div>;
+  }
+
+  if (error) {
+    return <div className="alert alert-danger text-center">Error</div>;
+  }
+
+  if (todos.length === 0) {
+    return (
+      <div className="alert alert-secondary text-center">No todos found.</div>
+    );
+  }
+
+  return (
+    <ul className="list-group">
+      {todos.map((todo) => (
+        <li
+          key={todo.id}
+          className={`list-group-item d-flex justify-content-between align-items-center ${
+            todo.completed ? "list-group-item-secondary" : ""
+          }`}
+          style={{ cursor: "pointer" }}
+          onClick={() => handleToggleTodo(todo)}
+        >
+          <span
+            style={{
+              textDecoration: todo.completed ? "line-through" : "none",
+            }}
+          >
+            {todo.text}
+          </span>
+          <div>
+            <button
+              onClick={(e) => handleCurrentTodo(todo.id, e)}
+              className="btn btn-warning btn-sm me-2"
+            >
+              {language === "english" ? "Edit" : "Ubah"}
+            </button>
+            <button
+              onClick={(e) => handleDeleteTodo(todo.id, e)}
+              className="btn btn-danger btn-sm"
+            >
+              {language === "english" ? "Delete" : "Hapus"}
+            </button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
